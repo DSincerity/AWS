f33860234e6009e7c858fa41396ae23e03474cd1
dskwon <dskwon@dskwonui-MacBook-Pro.local>
2019-09-09 11:28:43 +0900

    modified:add remove function

--- s3/remove_bucket_objects.py
+++ s3/remove_bucket_objects.py
@@ -23,3 +23,22 @@ def remove_bucket_objects(bucket_name:str, prefix_list:list):
             raise ResultError(f"There is an error in deleting object : {e}")
 
     return total_delete_obj_cnt
+
+
+def remove_bucket_object(bucket_name, prefix):
+    bucket_name = bucket_name
+    prefix = prefix
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name )
+    
+    delete_obj_cnt=0
+    for obj in bucket.objects.filter(Prefix=prefix):
+        response = bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': obj.key,
+                    }]})
+        #print(response['ResponseMetadata'])
+        delete_obj_cnt +=1
+    return delete_obj_cnt
